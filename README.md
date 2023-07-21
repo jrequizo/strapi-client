004e3886e5a9f67e90c44a4d355ce917cb6f1bd3
Jay Requizo <jay.requizo@outlook.com>
2023-07-21 17:38:24 +1000

    Update comments

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-WIP
+
+/**
+ * strapiClient default methods:
+ * Login:       POST: /api/auth/local
+ * Register:    POST: /api/auth/local/register
+ * 
+ * 
+ * pattern:
+ */
+
+/**
+ * ./api/strapi.ts
+ * 
+ * import { strapiClient } from "strapi-client";
+ * import models from "./strapi-models";
+ * 
+ * const api = strapiClient({
+ *      endpoint: "localhost", 
+ *      models: models,
+ * });
+ * 
+ * export default api;
+ */
+
+/**
+ * ./api/strapi-models.ts
+ * 
+ * import { StrapiModel, StrapiType } from "strapi-client";
+ * 
+ * const restaurantModel = StrapiModel("restaurant", {
+ *      name: StrapiType.string(),
+ * })
+ * .createDefaultRoutes()
+ * .createCustomRoute()
+ * 
+ * export const models = [
+ *      restaurantModel,
+ *      ...
+ * ]
+ */
+
+/**
+ * ./pages/main.tsx
+ * 
+ * import api from "@core/api/strapi"
+ * 
+ * async function getRestaurants() {
+ *      const restaurants = await api.restaurant.find();
+ * 
+ *      return restaurants;
+ * }
+ */
--- src/index.ts
+++ src/index.ts
@@ -6,93 +6,34 @@ export {
     StrapiModel
 };
 
-/**
- * Strapi type definitions.
- * 
- * Use this when defining the schema from the database.
- * Use a 1-to-1 mapping of the types on the database.
- */
+// import { z } from "zod";
 
+// const restaurantsModel = new StrapiModel("restaurants", {
+//     id: z.number(),
+//     name: z.string()
+// }).createDefaultRoutes();
 
-/**
- * strapiClient default methods:
- * Login:       POST: /api/auth/local
- * Register:    POST: /api/auth/local/register
- * 
- * 
- * pattern:
- */
+// const reviewsModel = new StrapiModel("reviews", {
+//     id: z.number(),
+//     rating: z.number()
+// }).createCustomRoutes("addReview", {
+//     async handler() {
+//         return {
+//             hello: "world!"
+//         }
+//     },
+// });
 
-/**
- * ./api/strapi.ts
- * 
- * import { strapiClient } from "strapi-client";
- * import models from "./strapi-models";
- * 
- * const api = strapiClient({
- *      endpoint: "localhost", 
- *      models: models,
- * });
- * 
- * export default api;
- */
+// const client = new StrapiClient({
+//     baseURL: 'localhost',
+//     models: [restaurantsModel, reviewsModel]
+// });
 
-/**
- * ./api/strapi-models.ts
- * 
- * import { StrapiModel, StrapiType } from "strapi-client";
- * 
- * const restaurantModel = StrapiModel("restaurant", {
- *      name: StrapiType.string(),
- * })
- * .createDefaultRoutes()
- * .createCustomRoute()
- * 
- * export const models = [
- *      restaurantModel,
- *      ...
- * ]
- */
+// async function run() {
+//     const result = await client.api.reviews.addReview();
+//     // client.api.restaurants.find()
 
-/**
- * ./pages/main.tsx
- * 
- * import api from "@core/api/strapi"
- * 
- * async function getRestaurants() {
- *      const restaurants = await api.restaurant.find();
- * 
- *      return restaurants;
- * }
- */
+//     console.log(result);
+// }
 
-import { z } from "zod";
-
-const restaurantsModel = new StrapiModel("restaurants", {
-    id: z.number(),
-    name: z.string()
-}).createDefaultRoutes();
-
-const reviewsModel = new StrapiModel("reviews", {
-    id: z.number(),
-    rating: z.number()
-}).createCustomRoutes("addReview", {
-    async handler() {
-        return {
-            hello: "world!"
-        }
-    },
-});
-
-const client = new StrapiClient({
-    baseURL: 'localhost',
-    models: [restaurantsModel, reviewsModel]
-});
-
-async function run() {
-    const result = await client.api.reviews.addReview();
-
-    console.log(result);
-}
-
-run();
+// run();
